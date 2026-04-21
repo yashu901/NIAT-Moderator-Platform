3dd4f4e6253ba92d7b320f1de2cc84b01bf021f8
yashu901 <somethingroy09@gmail.com>
2026-04-21 10:21:09 +0530

    connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://niat-moderator-platform.onrender.com/api/",
 });
 
 API.interceptors.request.use((req) => {
