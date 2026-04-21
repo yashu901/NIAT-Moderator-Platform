24f4302263d62ffb9f4bc7680a1cc6617ecd9386
yashu901 <somethingroy09@gmail.com>
2026-04-21 12:37:29 +0530

    fix login endpoint

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import login_view, me_view , send_invite, onboard
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path('login/', login_view),
+    path('login/', TokenObtainPairView.as_view()),
     path('me/', me_view),
     path('invite/', send_invite),
     path('onboard/', onboard),
